99a308184a9e55c9c75ef31006f8087e0946a83a
Rennish <rrenkish@gmail.com>
2023-02-25 16:08:01 -0800

    Writes_a function that prints an integer.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,26 @@
-#include "main.h"
-/**
- * positive_or_negative -checks for positive or negative numbers
- *
- * @i: the number to be checked
- *
- * Return always 0
- */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-void positive_or_negative(int i);
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
 {
-	if (i < 0)
-		printf("%d is negative", i);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
-		print("%d is positive", i);
+		printf("%d is zero\n", n);
+
+	return (0);
 }
