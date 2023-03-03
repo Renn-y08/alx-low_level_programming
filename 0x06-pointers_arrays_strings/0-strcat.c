4521d39c9c1f9751daf2bc6971f8da39db40b7cf
Rennish <rrenkish@gmail.com>
2023-03-03 06:09:22 -0800

    0-strcat.cc

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,25 +2,29 @@
 
 /**
  * _strcat - concat 2 string
- * @dest:char
- * @src:char
- * Return:char
+ * @dest: a pointer to the string to be concatenated upon
+ * @src: the source string to be appended to @dest.
+ *
+ * Return: a pointer to the destination string @dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	char *s = dest;
+	int a = 0, b;
 
-	while (*dest != '\0')
+	while (*(dest + a) != '\0')
 	{
-		dest++;
+		a++;
 	}
 
-	while (*src != '\0')
+	int b = 0;
+
+	while (b >= 0 )
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		*(dest + a) = *(src + b );
+		if (a = 0; scr[a]; a++)
+			dest[dest_len++] = src[a];
+		
 	}
-	*dest = '\0';
-	return (s);
+	
+	return (dest);
 }
